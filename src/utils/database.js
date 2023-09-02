c596dd9dc58d055a2de14f0c604a52dcad984d4e
GerardoAcosta1 <gerardonuevo1234321@gmail.com>
2023-09-01 20:04:36 -0700

    nada

--- src/utils/database.js
+++ src/utils/database.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 
 const db = new Sequelize (
     {
-        host: 'localhost',
+        host: 'postgres://tareas_ux36_user:lpvu6kGHS6syOoE4FAy1v2GNtzFPC1rT@dpg-cjpa6iojbais7381tk10-a/tareas_ux36',
         username: 'postgres',
         database: 'tareas',
         port: '5432',
